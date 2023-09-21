b102c5f5763a5308339bd7d41f02de2734c32446
TACH777AMEN <gabo07674@gmail.com>
2023-09-20 23:04:46 -0500

    Update

--- session.js
+++ session.js
@@ -15,3 +15,48 @@ function verificarSesion() {
 function obtenerUsuarioActual() {
     return usuarioActual;
 }
+
+// Datos de usuarios y contraseñas
+var usuarios = {
+    "tachamen": {
+        "contrasena": "456123",
+        "zona": "zona1"
+    },
+    "22": {
+        "contrasena": "22",
+        "zona": "zona2"
+    },
+    "333": {
+        "contrasena": "333",
+        "zona": "zona3"
+    },
+    "4444": {
+        "contrasena": "4444",
+        "zona": "zona4"
+    },
+    "55555": {
+        "contrasena": "55555",
+        "zona": "zona5"
+    },
+    "666666": {
+        "contrasena": "666666",
+        "zona": "zona6"
+    },
+    "7777777": {
+        "contrasena": "7777777",
+        "zona": "zona7"
+    },
+    "88888888": {
+        "contrasena": "88888888",
+        "zona": "zona8"
+    },
+    "999999999": {
+        "contrasena": "999999999",
+        "zona": "zona9"
+    },
+    "usuario10": {
+        "contrasena": "contraseña10",
+        "zona": "zona10"
+    }
+};
+
--- usuarios.js
+++ usuarios.js
@@ -1,12 +1,42 @@
 var usuarios = {
-    "TACH": "123456*",
-    "usuario2": "contraseña2",
-    "usuario3": "contraseña3",
-    "usuario4": "contraseña4",
-    "usuario5": "contraseña5",
-    "usuario6": "contraseña6",
-    "usuario7": "contraseña7",
-    "usuario8": "contraseña8",
-    "usuario9": "contraseña9",
-    "usuario10": "contraseña10"
+    "tachamen": {
+        "contrasena": "456123",
+        "zona": "zona1",
+    },
+    "22": {
+        "contrasena": "22",
+        "zona": "zona2",
+    },
+    "333": {
+        "contrasena": "333",
+        "zona": "zona3",
+    },
+    "4444": {
+        "contrasena": "4444",
+        "zona": "zona4",
+    },
+    "55555": {
+        "contrasena": "55555",
+        "zona": "zona5"
+    },
+    "666666": {
+        "contrasena": "666666",
+        "zona": "zona6"
+    },
+    "7777777": {
+        "contrasena": "7777777",
+        "zona": "zona7"
+    },
+    "88888888": {
+        "contrasena": "88888888",
+        "zona": "zona8"
+    },
+    "999999999": {
+        "contrasena": "999999999",
+        "zona": "zona9"
+    },
+    "usuario10": {
+        "contrasena": "contraseña10",
+        "zona": "zona10"
+    }
 };
